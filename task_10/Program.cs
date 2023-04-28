ebcef1f7ecdcb6934b0e3a3cb99e9b9de7ac9a9c
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-04-29 01:17:48 +0300

    Solution of the task 10 with Method ShowSecondFigure

---
+++ task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Например:
+// 456 --> 5
+// 782 --> 8
+// 918 --> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+ShowSecondFigure(number1);
+
+
+void ShowSecondFigure(int number)
+{
+    int result = number % 100 / 10;
+    Console.WriteLine("Second figure this number: " + result);
+}
+
