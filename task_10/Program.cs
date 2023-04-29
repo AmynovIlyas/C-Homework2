6df67df9250575fbea3570e1bb8bc3f8a24a8db1
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-04-29 22:51:21 +0300

    Correct solution of the tsak 10

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -6,12 +6,32 @@
 
 Console.WriteLine("Введите трёхзначное число: ");
 int number1 = int.Parse(Console.ReadLine()!);
+number1 = NumberSelection(number1);
 ShowSecondFigure(number1);
 
+int NumberSelection(int number)
+{
+    if (number < 100 | number > 999)
+    {
+        return 0;
+    }
+    else
+    {
+        return number;
+    }
+}
 
 void ShowSecondFigure(int number)
 {
-    int result = number % 100 / 10;
-    Console.WriteLine("Second figure this number: " + result);
+    if (number != 0)
+    {
+        int result = number % 100 / 10;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Введённое число не  является трёхзначным");
+    }
 }
+
 
