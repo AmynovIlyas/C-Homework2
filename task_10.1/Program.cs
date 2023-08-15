5cff59c3fe2e3b330c9d838586321b8f61da8e17
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-08-15 14:53:42 +0300

    new solution of the task 10.1 and 15.1

---
+++ task_10.1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Например:
+// 456 --> 5
+// 782 --> 8
+// 918 --> 1
+
+
+int number = Prompt("Введите трёхзначное число: ");
+if (CheckDigit(number))
+{
+    ShowSecondFigure(number);
+}
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+bool CheckDigit(int num)
+{
+    if(num < 100 | num > 999)
+    {
+        Console.WriteLine("Введённое число не является трёхзначным");
+        return false;
+    }
+    return true;
+}
+
+void ShowSecondFigure(int num)
+{
+    int result = num % 100 / 10;
+    Console.WriteLine($"{num} --> {result}"); 
+}
+
---
+++ task_15.1/Program.cs
@@ -0,0 +1,76 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+// Например:
+// 6 --> да
+// 7 --> да
+// 1 --> нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int number1 = Prompt("Введите цифру, обозначающую день недели: ");
+if (DayOfTheWeek(number1))
+{
+    WeekendOrNot(number1);
+}
+
+void WeekendOrNot(int number)
+{
+    if (number > 5)
+    {
+        Console.WriteLine("Ура! Выходной!");
+    }
+    else 
+    {
+        Console.WriteLine("Придётся поработать");
+    }
+}
+
+bool DayOfTheWeek(int number)
+{
+    if (number > 7 | number < 1)
+    {
+        Console.WriteLine("Это не день недели!");
+        return false; 
+    }
+    return true;
+}
+
+
+
+// int number = Prompt("Введите трёхзначное число: ");
+// if (number < 100 | number > 999)
+// {
+//     Console.WriteLine("Вы ввели не трёхзначное число, пожалуйста, повторите ввод");
+//     return;
+// }
+// Console.WriteLine($"Вторая цифра введённого числа --> {number / 10 % 10}");
+
+
+// int GetThirdRank(int number)
+// {
+//     while (number > 999)
+//     {
+//         number /= 10;
+//     }
+//     return number % 10;
+// }
+
+// bool ValidateNumber(int number)
+// {
+//     if (number < 100)
+//     {
+//         Console.WriteLine("Третьей цифры нет");
+//         return false;
+//     }
+//     return true;
+// }
+
+// int number = Prompt("Введите число: ");
+// if (ValidateNumber(number))
+// {
+//     Console.WriteLine(GetThirdRank(number));
+// }
